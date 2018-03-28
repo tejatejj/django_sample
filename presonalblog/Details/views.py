555c09678540f4c5875b459a9bc491bb8214ce04
tejatejj <avrteja123@gmail.com>
2018-03-28 19:07:44 +0000

    fourth commit

--- presonalblog/Details/views.py
+++ presonalblog/Details/views.py
@@ -1,11 +1,15 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+import random
 from django.shortcuts import render
 from django.http import HttpResponse
 
 
 def home(request):
-    html_var='my name ia teja'
-    #return HttpResponse('hello')
-    return render(request,"base.html")
+    html_var=1000
+    num=random.randint(0,190000)
+    some_list=[num,html_var,random.randint(0,1200)]
+    return render(request,"base.html",{"bool_item":False,"num":num,"some_list":some_list})
+
+
+
